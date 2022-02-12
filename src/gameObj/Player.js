8bfb5975cf0168aad2b4684304e2ceba38dd56f2
danielyoungkimball <danielyoungkimball@outlook.com>
2022-02-11 22:09:20 -0800

    eat my butt

--- src/gameObj/Player.js
+++ src/gameObj/Player.js
@@ -6,13 +6,16 @@ import dango from "./dango.png"
 export default function Player() {
   const pos = useRecoilValue(posState);
 
+  let style;
   useEffect(() => {
     console.log(pos)
+    style = "left: " + pos.toString() + "%";
+    console.log(style)
   });
 
   console.log(pos)
   return (
-    <div className="Player" style={{ left: `${pos}` + "%" }}>
+    <div className="Player" css={style}>
       <img src={dango} alt="dango" className="dango" />
     </div>
   );
